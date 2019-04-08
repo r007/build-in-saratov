6ec696b4035db2db9ed0f4dc24051109e4b61de7
r007 <moninsergei@gmail.com>
2019-04-09 01:50:46 +0400

    Site nav refactor

--- src/components/header/SiteNav.tsx
+++ src/components/header/SiteNav.tsx
@@ -2,7 +2,6 @@
 import { Link } from 'gatsby';
 import * as React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/core';
 
 import { SocialLink } from '../../styles/shared';
 import config from '../../website-config';
@@ -11,78 +10,70 @@ import Twitter from '../icons/twitter';
 import SubscribeModal from '../subscribe/SubscribeOverlay';
 import SiteNavLogo from './SiteNavLogo';
 
-const HomeNavRaise = css`
-  @media (min-width: 900px) {
-    position: relative;
-    top: -70px;
-  }
-`;
-
-const SiteNavStyles = css`
-  position: relative;
-  z-index: 300;
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-  overflow-y: hidden;
-  height: 40px;
-  font-size: 1.2rem;
-`;
-
-const SiteNavLeft = styled.div`
-  display: flex;
-  align-items: center;
-  overflow-x: auto;
-  overflow-y: hidden;
+const SiteNavWrapper = styled.nav`
+  left: 0;
+  position: fixed;
+  display: block;
+  top: 0;
+  max-width: 15rem;
+  width: 15rem;
+  background: #1a1f3e;
+  height: 100%;
+  z-index: 950;
+  padding: 1rem;
+  box-sizing: border-box;
+  overflow-y: auto;
+  color: #bfddff;
   -webkit-overflow-scrolling: touch;
-  margin-right: 10px;
-  padding-bottom: 80px;
-  letter-spacing: 0.4px;
-  white-space: nowrap;
-
-  -ms-overflow-scrolling: touch;
-
-  @media (max-width: 700px) {
-    margin-right: 0;
-    padding-left: 4vw;
-  }
+  -webkit-transform: translate3d(0,0,0);
+  transition: all .3s cubic-bezier(.645,.045,.355,1);
 `;
 
-const NavStyles = css`
-  display: flex;
-  margin: 0 0 0 -12px;
+const Wrapper = styled.div`
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+`;
+
+const NavWrapper = styled.div`
+  padding-bottom: 7rem;
+  min-height: 100%;
+  position: relative;
+  box-sizing: border-box;
+  overflow-y: hidden;
+`;
+
+const NavLink = styled(Link)`
+  display: block;
+  float: none;
+  font-size: 20px;
+  letter-spacing: .05em;
+  margin: 1em 1em 1em 0;
   padding: 0;
-  list-style: none;
+  position: relative;
 
-  li {
-    display: block;
-    margin: 0;
-    padding: 0;
-    text-transform: uppercase;
-  }
-
-  li a {
-    display: block;
-    margin: 0;
-    padding: 10px 12px;
-    color: #fff;
-    opacity: 0.8;
-  }
-
-  li a:hover {
+  :hover {
     text-decoration: none;
-    opacity: 1;
   }
-`;
 
-const SiteNavRight = styled.div`
-  flex-shrink: 0;
-  display: flex;
-  align-items: center;
-  height: 40px;
+  &[aria-current~="page"]::after {
+    position: absolute;
+    width: .8em;
+    right: .5em;
+  }
 
-  @media (max-width: 700px) {
-    display: none;
+  &[aria-current~="page"] {
+    background: #aad2ff;
+    box-sizing: border-box;
+    color: #1a1f3e;
+    letter-spacing: .03em;
+    padding: .3em 0 .4em .6em;
+    width: 100%;
   }
 `;
 
@@ -112,21 +103,9 @@ const SubscribeButton = styled.a`
   }
 `;
 
-interface SiteNavProps {
-  isHome?: boolean;
-}
-
-interface SiteNaveState {
-  isOpen: boolean;
-}
-
-class SiteNav extends React.Component<SiteNavProps, SiteNaveState> {
+class SiteNav extends React.Component {
   subscribe = React.createRef<SubscribeModal>();
 
-  constructor(props: SiteNavProps) {
-    super(props);
-    this.state = { isOpen: false };
-  }
   openModal = () => {
     if (this.subscribe.current) {
       this.subscribe.current.open();
@@ -134,55 +113,46 @@ class SiteNav extends React.Component<SiteNavProps, SiteNaveState> {
   };
 
   render() {
-    const { isHome = false } = this.props;
     return (
-      <nav css={[isHome && HomeNavRaise, SiteNavStyles]}>
-        <SiteNavLeft>
-          {!isHome && <SiteNavLogo />}
-          <ul css={NavStyles} role="menu">
-            {/* TODO: mark current nav item - add class nav-current */}
-            <li role="menuitem">
-              <Link to="/">Home</Link>
-            </li>
-            <li role="menuitem">
-              <Link to="/about">About</Link>
-            </li>
-            <li role="menuitem">
-              <Link to="/tags/getting-started/">Getting Started</Link>
-            </li>
-          </ul>
-        </SiteNavLeft>
-        <SiteNavRight>
-          <SocialLinks>
-            {config.facebook && (
-              <a
-                css={SocialLink}
-                href={config.facebook}
-                target="_blank"
-                title="Facebook"
-                rel="noopener noreferrer"
-              >
-                <Facebook />
-              </a>
+      <SiteNavWrapper>
+        <Wrapper>
+          <NavWrapper>
+            <SiteNavLogo />
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/tags/getting-started/">Getting Started</NavLink>
+
+            <SocialLinks>
+              {config.facebook && (
+                <a
+                  css={SocialLink}
+                  href={config.facebook}
+                  target="_blank"
+                  title="Facebook"
+                  rel="noopener noreferrer"
+                >
+                  <Facebook />
+                </a>
+              )}
+              {config.twitter && (
+                <a
+                  css={SocialLink}
+                  href={config.twitter}
+                  title="Twitter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Twitter />
+                </a>
+              )}
+            </SocialLinks>
+            {config.showSubscribe && (
+              <SubscribeButton onClick={this.openModal}>Subscribe</SubscribeButton>
             )}
-            {config.twitter && (
-              <a
-                css={SocialLink}
-                href={config.twitter}
-                title="Twitter"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Twitter />
-              </a>
-            )}
-          </SocialLinks>
-          {config.showSubscribe && (
-            <SubscribeButton onClick={this.openModal}>Subscribe</SubscribeButton>
-          )}
-          {config.showSubscribe && <SubscribeModal ref={this.subscribe} />}
-        </SiteNavRight>
-      </nav>
+            {config.showSubscribe && <SubscribeModal ref={this.subscribe} />}
+          </NavWrapper>
+        </Wrapper>
+      </SiteNavWrapper>
     );
   }
 }
