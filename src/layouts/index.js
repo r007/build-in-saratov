35c38ec4c05ba0759fb2cce109a6de107b711027
r007 <moninsergei@gmail.com>
2019-04-29 05:01:41 +0400

    Fix section on small displays

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -42,6 +42,18 @@ const GlobalStyles = css`
     position: relative;
     padding: 3rem 3rem 0 20rem;
     overflow: hidden;
+    
+    @media only screen and (max-width: 1560px) {
+      padding: 1rem 2rem 0 15rem
+    }
+
+    @media only screen and (max-width: 1280px) {
+      padding: 2rem 3rem 0 3.4rem;
+    }
+
+    @media only screen and (max-width: 460px) {
+      padding: 1rem 1rem 0 1rem;
+    }
   }
 
   section blockquote,
