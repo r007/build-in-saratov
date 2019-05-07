3166befd07038f619b7139323f6a246d8cf16eda
r007 <moninsergei@gmail.com>
2019-05-08 01:17:11 +0400

    Track outbound links with google gtag

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { OutboundLink } from 'gatsby-plugin-google-gtag';
 
-const SocialLink = styled.a`
+const SocialLink = styled(OutboundLink)`
   display: flex;
   justify-content: center;
   align-items: center;
