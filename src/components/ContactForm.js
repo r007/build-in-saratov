2580f464b095f8b0c5fc72a09d2e78342d11c5fd
r007 <moninsergei@gmail.com>
2019-05-09 13:11:50 +0400

    Change endPoint stage to prod

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -188,7 +188,7 @@ class ContactForm extends React.Component {
           </form>
         )}
         onSubmit={({ fullName, email, message }, actions) => {
-          const endPoint = 'https://qveaqjxu0g.execute-api.us-east-1.amazonaws.com/dev';
+          const endPoint = 'https://qveaqjxu0g.execute-api.us-east-1.amazonaws.com/prod';
 
           axios
             .post(`${endPoint}/contact`, {
