45ec01cde0cd7c58140d54a69a93f50f5db1803d
r007 <moninsergei@gmail.com>
2021-12-05 07:51:15 +0400

    Update all the dependencies, remove gatsby feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,6 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
-    'gatsby-plugin-feed',
     // SEO-related functionality
     'gatsby-plugin-advanced-sitemap',
     'gatsby-plugin-robots-txt',
